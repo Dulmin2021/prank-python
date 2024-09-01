38dc60c533789f481e3a00b7f5716c40499db293
Dulmin wickramage <92096416+Dulmin2021@users.noreply.github.com>
2024-09-01 22:11:32 +0530

    Create ghost.py

---
+++ ghost.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+import random
+
+def typing_ghost():
+    sentences = [
+        "Why are you ignoring me?",
+        "I'm still here...",
+        "You can't escape me.",
+        "I'm watching you.",
+        "Did you hear that noise?",
+    ]
+    while True:
+        time.sleep(random.uniform(5, 15))  # Random delay between sentences
+        pyautogui.typewrite(random.choice(sentences))
+        pyautogui.press('enter')
+
+typing_ghost()
